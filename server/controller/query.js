43bf000c6c2e2dabedf2a30e3d81e0f4cbc3a569
Mr.Superb <supravbajracharya.ths@gmail.com>
2024-12-26 15:02:26 +0545

    Renamed env vairable
    Signed-off-by: Mr.Superb <supravbajracharya.ths@gmail.com>

--- server/controller/query.js
+++ server/controller/query.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const optimize = async (req, res, next) => {
   try {
